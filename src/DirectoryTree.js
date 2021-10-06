a89e74380dea1ccf085676e0f994a9d61e129c37
Hugo Vargas <vargas88hugo@gmail.com>
2021-10-05 19:03:07 -0500

    feat: implement composite pattern to directory tree

--- src/DirectoryTree.js
+++ src/DirectoryTree.js
@@ -1,80 +1,11 @@
-import { DoesNotExistException } from './utils/exceptions';
-
-class DirectoryTree {
-  constructor (logger) {
-  	this.directoryTree = new Map();
-    this.logger = logger;
+export default class DirectoryTree {
+  constructor(name, composites = []) {
+    this.name = name;
+    this.composites = composites.sort((x, y) => x.name - y.name);
   }
 
-  create (path, directory) {
-  	this._validatePath(path);
-  	const { workspace, baseName } = this._splitPath(path);
-  	const keyValueDirectory = [baseName, directory || new Map()];
-  	this._createDirectory(workspace, keyValueDirectory);
-  }
-
-  move (source, destiny) {
-  	const deletedDirectory = this.delete(source);
-  	this.create(destiny, deletedDirectory);
-  }
-
-  delete (path) {
-  	this._validatePath(path);
-  	const { workspace, baseName } = this._splitPath(path);
-  	const deletedDirectory = this._deleteDirectory(workspace, baseName);
-  	return deletedDirectory;
-  }
-
-  list () {
-  	this._printDirectory(this.directoryTree, '');
-  }
-
-  _printDirectory (directoryTree, space) {
-  	const entries = Array.from(directoryTree.entries());
-  	for (const entry of entries.sort()) {
-  		const [entryKey, entryValue] = entry;
-  		this.logger.log(space + entryKey);
-      this._printDirectory(entryValue, space + ' ');
-  	}
-  }
-
-  _validatePath (path) {
-  	if (!path) throw new DoesNotExistException('path');
-  }
-
-  _validateSubDirectory (subdirectory) {
-  	if (!subdirectory) throw new DoesNotExistException('subdirectory');
-  }
-
-  _createDirectory (workspace, keyValueDirectory) {
-  	const [key, value] = keyValueDirectory;
-  	const workspaceMap = this._getWorkspace(workspace);
-  	const oldValues = workspaceMap.get(key);
-  	workspaceMap.set(key, (oldValues) ? new Map([...value, ...oldValues]) : value);
-  }
-
-  _deleteDirectory (workspace, baseName) {
-  	const workspaceMap = this._getWorkspace(workspace);
-  	const directoryCopy = (workspace.length > 0) ? new Map(workspaceMap) : new Map([[baseName, workspaceMap.get(baseName)]]);
-  	workspaceMap.delete(baseName);
-  	return directoryCopy;
-  }
-
-  _getWorkspace (workspace) {
-  	let currentSubdirectory = this.directoryTree;
-  	for (const subdirectory of workspace) {
-  		currentSubdirectory = currentSubdirectory.get(subdirectory);
-  		this._validateSubDirectory(currentSubdirectory);
-  	}
-  	return currentSubdirectory;
-  }
-
-  _splitPath (path) {
-  	const splitPath = path.split('/');
-  	const workspace = splitPath.slice(0, -1);
-  	const baseName = splitPath[splitPath.length - 1];
-  	return { workspace, baseName };
+  print(tab = '') {
+    console.log(`${tab}${this.name}`);
+    this.composites.forEach(item => item.print(tab + ' '));
   }
 }
-
-export default DirectoryTree;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-import 'dotenv/config';
-import path from 'path';
-import FileReader from './FileReader';
-import DirectoryTree from './DirectoryTree';
-import InstructionHandler from './InstructionHandler';
-import { COMMANDS_MAP } from './utils/constants';
-import logger from './utils/logger.js';
+import DirectoryTree from "./DirectoryTree";
 
-const fileReader = new FileReader(logger);
-const filePath = path.resolve(__dirname, process.env.FILE_PATH);
-const instructions = fileReader.readFile(filePath);
+const nestedOne = new DirectoryTree('nestedOne');
+const nestedTwo = new DirectoryTree('nestedTwo');
+const one = new DirectoryTree('one', [nestedOne, nestedTwo]);
+const two = new DirectoryTree('two');
+const root = new DirectoryTree('root', [one, two]);
 
-const directoryTree = new DirectoryTree(logger);
-const instructionHandler = new InstructionHandler(directoryTree, logger);
-const executionMap = new Map();
-
-executionMap.set(COMMANDS_MAP.create, directoryTree.create);
-executionMap.set(COMMANDS_MAP.delete, directoryTree.delete);
-executionMap.set(COMMANDS_MAP.move, directoryTree.move);
-executionMap.set(COMMANDS_MAP.list, directoryTree.list);
-instructionHandler.executionMap = executionMap;
-
-instructionHandler.processInstructions(instructions);
+root.print();
